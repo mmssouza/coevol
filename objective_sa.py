be83907e9b560555416f7f64c066fdcee05c401e
Marcelo M. S. Souza <marcelo.mssouza@gmail.com>
2015-10-16 08:41:03 -0300

    Corrected errors

--- objective_sa.py
+++ objective_sa.py
@@ -9,15 +9,6 @@ import descritores as desc
 import optimize
 import cost_func_mt
 
-fd = open (sys.argv[1],'wb') 
-optimize.set_dim(7)
-sa = optimize.sim_ann(cost_func_mt.cost_func,0.125+ np.random.rand(optimize.Dim) + 25,180,0.95,10,5)
-for j in range(3):
- for i in range(1000):
-  sa.run()
-   
-fd.close()   
-
 N,M = 1250,3
 with open(sys.argv[1],"wb") as f:
  cPickle.dump((N,M),f)
