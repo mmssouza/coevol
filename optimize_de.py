ddcaf5c3eb5448928eac8a194297b2f0f3a2fd0a
Marcelo@Sony notebook <marcelo.mssouza@gmail.com>
2015-11-03 14:10:52 -0300

    Parse optimize scripts parameters using getopt

--- optimize_de.py
+++ optimize_de.py
@@ -1,29 +1,57 @@
 #!/usr/bin/python -u
 # -*- coding: utf-8 -*-
 import sys
+import getopt
 import optimize
-import cost_func_mt
 import scipy
 import cPickle
 
-algo = "de"
-conf = [30,0.4,0.75]
-dim = 7
-dataset = "leaves_160_png"
-N,M = 1000,3
+mt = False
+dataset = ""
+fout = ""
+dim = -1
+try:                                
+ opts,args = getopt.getopt(sys.argv[1:], "o:d:", ["mt","dim=","output=","dataset="])
+except getopt.GetoptError:           
+ print "Error getopt"                          
+ sys.exit(2)          
+ 
+for opt,arg in opts:
+ if opt == "--mt":
+  mt = True
+ elif opt in ("-o","--output"):
+  fout = arg
+ elif opt in ("-d","--dataset"):
+  dataset = arg
+ elif opt == "--dim":
+  dim = int(arg) 
+  
+conf = [float(i) for i in args]
 
-Head = {'algo':"algo: "+algo,'conf':" npop = {0}, pr = {1}, beta = {2}".format(conf[0],conf[1],conf[2]),'dim':"n = {0}".format(dim),"dataset":"dataset: "+dataset}
+if dataset == "" or fout == "" or len(conf) != 3 or dim <= 0:
+ print "Error getopt" 
+ sys.exit(2)
+
+if mt:
+ from cost_func_mt import cost_func,DatasetLoad
+else:
+ from cost_func import cost_func,DatasetLoad
+ 
+algo = "de"
+N,M = 1200,3
+
+Head = {'algo':algo,'conf':" npop = {0}, pr = {1}, beta = {2}".format(conf[0],conf[1],conf[2]),'dim':dim,"dataset":dataset}
 
 optimize.set_dim(dim)
 
-cost_func_mt.DatasetLoad("../"+dataset+"/")
+DatasetLoad(dataset+"/")
 
 if __name__ == '__main__':
  with open(sys.argv[1],"wb") as f:
   cPickle.dump(Head,f)
   cPickle.dump((N,M),f)
   for j in range(M):
-   v = optimize.de(cost_func_mt.cost_func,conf[0],conf[1],conf[2])
+   v = optimize.de(cost_func,conf[0],conf[1],conf[2])
    for i in range(N):
     v.run()
     print i,v.fit.min()
--- optimize_pso.py
+++ optimize_pso.py
@@ -1,21 +1,50 @@
 #!/usr/bin/python -u
 # -*- coding: utf-8 -*-
 import sys
+import getopt
 import optimize
 import cost_func_mt
 import cPickle
 
-algo = "pso"
-conf = [25,0.965,2.68,2.68]
-dim = 5
-dataset = "leaves_160_png"
-N,M = 100,3
+mt = False
+dataset = ""
+fout = ""
+dim = -1
 
-Head = {'algo':"algo: "+algo,'conf':"npop = {0}, w = {1}, c1 = {2}, c2 = {3}".format(conf[0],conf[1],conf[2],conf[3]),'dim':"n = {0}".format(dim),"dataset":"dataset: "+dataset}
+try:                                
+ opts,args = getopt.getopt(sys.argv[1:], "o:d:", ["mt","dim =","output=","dataset="])
+except getopt.GetoptError:           
+ print "Error getopt"                          
+ sys.exit(2)          
+ 
+for opt,arg in opts:
+ if opt == "--mt":
+  mt = True
+ elif opt in ("-o","--output"):
+  fout = arg
+ elif opt in ("-d","--dataset"):
+  dataset = arg
+ elif opt == "--dim":
+  dim = int(arg)
+conf = [float(i) for i in args]
+
+if dataset == "" or fout == "" or len(conf) != 4 or dim <= 0:
+ print "Error getopt" 
+ sys.exit(2)
+
+if mt:
+ from cost_func_mt import cost_func,DatasetLoad
+else:
+ from cost_func import cost_func,DatasetLoad
+
+algo = "pso"
+N,M = 1200,3
+
+Head = {'algo':algo,'conf':"npop = {0}, w = {1}, c1 = {2}, c2 = {3}".format(conf[0],conf[1],conf[2],conf[3]),'dim':dim,"dataset":dataset}
 
 optimize.set_dim(dim)
 
-cost_func_mt.DatasetLoad("../"+dataset+"/")
+cost_func_mt.DatasetLoad(dataset+"/")
 
 if __name__ == '__main__':
  with open(sys.argv[1],"wb") as f:
