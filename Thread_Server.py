3b8dfc6eaeebaf09a1cbe40a36c930ebd89f9ebd
Marcelo M. S. Souza <marcelo.mssouza@gmail.com>
2016-03-24 20:42:35 -0300

    added method to extract contour using pool of workers

--- Thread_Server.py
+++ Thread_Server.py
@@ -1,20 +1,42 @@
 import rpyc
 import sys
 from threading import Thread,Event
+from multiprocessing import Pool
 import oct2py
 import numpy as np
+from functools import partial
 
 rpyc.core.protocol.DEFAULT_CONFIG['allow_pickle'] = True
-sys.path.append("c:\\Users\\marce\\prj\\coevol")
-	
+sys.path.append("/home/isaura/prj/coevol")
+
+
+#def f(n,nc):
+#          oc = oct2py.Oct2Py()
+#          im = oc.imread("../leaves_png/"+n)
+#          s = oc.extract_longest_cont(im,nc)
+#          oc.exit()
+#          return np.array([complex(i[0],i[1]) for i in s]) 
+
+def f(n,nc):
+          oc = oct2py.Oct2Py()
+          oc.push(['name','nc'],["../leaves_png/"+n,nc])
+          oc.eval(["im = imread(name);","c = extract_longest_cont(im,nc);"],log = False)
+          s = oc.pull("c")
+          oc.exit()
+          return np.array([complex(i[0],i[1]) for i in s]) 
+
 class MyService(rpyc.Service):
     	 def on_connect(self):
-           pass
-
+           self.pool = Pool(processes=4)         
+           
          def on_disconnect(self):
-          pass
-         
-         
+           self.pool.close()
+
+         def exposed_evalcontours(self,n,nc):
+           contours = self.pool.map(partial(f,nc = nc),n)
+           return contours
+                   
+
          class exposed_EvalContours(object):   
             def __init__(self,names,nc = 256):
                         self.names = names
@@ -30,6 +52,9 @@ class MyService(rpyc.Service):
  
             def exposed_active(self): 
              self.active.set()
+
+            def exposed_set_nc(self,nc):
+             self.nc = nc
  
             def exposed_is_busy(self):
               return self.active.is_set()
@@ -40,7 +65,7 @@ class MyService(rpyc.Service):
               self.thread.join()
 
             def work(self):
-             oc = oct2py.Oct2Py(temp_dir = "c:\\users\\marce\\prj\\coevol\\tmp") 
+             oc = oct2py.Oct2Py() 
              while True:
               self.active.wait()
               if self.stop == True: 
@@ -49,7 +74,7 @@ class MyService(rpyc.Service):
          
               contours = []
               for k in self.names: 
-  	           im = oc.imread("c:\\Users\\marce\\prj\\leaves_png\\"+k)
+  	           im = oc.imread("../leaves_png/"+k)
   	           s = oc.extract_longest_cont(im,self.nc)
   	           contours.append(np.array([complex(i[0],i[1]) for i in s]))
               self.exposed_contours = contours
