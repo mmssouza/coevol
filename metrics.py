added option to choose the distance foor silhouette calculation. bug corrected in sa hall of fame. Added mahalanobis distance to silhouette on fitness calculation and show hall of fame after all iteration

metrics.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy.spatial.distance import pdist,squareform,euclidean
 #import distances
 
-def silhouette(X, cIDX):
+def silhouette(X, cIDX,distance = 'euclidean'):
     """
     Computes the silhouette score for each instance of a clustered dataset,
     which is defined as:
@@ -22,7 +22,7 @@ def silhouette(X, cIDX):
     K = len(np.unique(cIDX))    # number of clusters
 
     # compute pairwise distance matrix
-    D = squareform(pdist(X))
+    D = squareform(pdist(X,distance))
 
     # indices belonging to each cluster
     kIndices = [np.flatnonzero(cIDX==k) for k in range(K)]
@@ -34,7 +34,7 @@ def silhouette(X, cIDX):
         # instances in same cluster other than instance itself
         a[i] = np.mean( [D[i][ind] for ind in kIndices[cIDX[i]] if ind!=i] )
         # instances in other clusters, one cluster at a time
-        b[i] = np.min( [np.mean(D[i][ind]) 
+        b[i] = np.min( [np.mean(D[i][ind])
                         for k,ind in enumerate(kIndices) if cIDX[i]!=k] )
     s = (b-a)/np.maximum(a,b)
 
@@ -47,8 +47,8 @@ def delta(A,B,dist):
   return d[Na:Na+Nb,0:Na].min()
 
 def Delta(A,dist):
-   return pdist(A,metric=dist).max() 
- 
+   return pdist(A,metric=dist).max()
+
 # Dunn measure
 def di(X,cIDX,distance = 'euclidean'):
   Nclusters = cIDX.max()
@@ -59,8 +59,8 @@ def di(X,cIDX,distance = 'euclidean'):
   for i in range(1,Nclusters+1):
    for j in range(1,Nclusters+1):
      if i != j:
-      aux2.append(delta(X[np.where(cIDX == i)],X[np.where(cIDX == j)],distance)/aux)    
-   aux3.append(np.array(aux2).min())     
+      aux2.append(delta(X[np.where(cIDX == i)],X[np.where(cIDX == j)],distance)/aux)
+   aux3.append(np.array(aux2).min())
   return np.array(aux3).min()
 
 # David-Bouldin's measure (db)
@@ -71,17 +71,17 @@ def db(X,cIDX,q = 1,t = 2,distance = 'euclidean'):
  A = np.array([ X[np.where(cIDX == i)] for i in range(Nclusters)])
 # Centroids
  v = np.array([ np.sum(Ai,axis = 0)/float(Ai.shape[0])  for Ai in A])
- 
+
  s = []
  for Ai,vi in zip(A,v):
-  s.append((np.array([euclidean(x,vi)**float(q)  for x in Ai]).sum()/float(Ai.shape[0]))**(1/float(q))) 
- 
+  s.append((np.array([euclidean(x,vi)**float(q)  for x in Ai]).sum()/float(Ai.shape[0]))**(1/float(q)))
+
  d = squareform(pdist(v,'minkowski',t))
- 
+
  R = []
  for i in range(Nclusters):
    R.append(np.array([(s[i] + s[j])/d[i,j] for j in range(Nclusters) if j != i]).max())
- 
+
  return np.array(R).sum()/float(Nclusters)
 
 # CS index : ratio of the sum of within-cluster scatter to between cluster separation.
@@ -93,8 +93,8 @@ def CS(X,cIDX,dist='euclidean'):
 # Clusters
  A = np.array([ X[np.where(cIDX == i)] for i in range(Nclusters)])
 # Centroids
- v = np.array([ np.sum(Ai,axis = 0)/float(Ai.shape[0])  for Ai in A]) 
- dv = squareform(pdist(v, metric = dist)) 
+ v = np.array([ np.sum(Ai,axis = 0)/float(Ai.shape[0])  for Ai in A])
+ dv = squareform(pdist(v, metric = dist))
  aux1 = []
  aux2 = []
  for i in range(Nclusters):
@@ -115,11 +115,11 @@ def MM(X,cIDX, m = 2):
 # Clusters
  A = np.array([ X[np.where(cIDX == i)] for i in range(Nclusters)])
 # Centroids
- v = np.array([ np.sum(Ai,axis = 0)/float(Ai.shape[0])  for Ai in A]) 
+ v = np.array([ np.sum(Ai,axis = 0)/float(Ai.shape[0])  for Ai in A])
 
  for i in range(Nclusters):
   for j in range(Npoints):
-    M[i,j] = 1/np.array([(euclidean(X[j],v[i])/euclidean(X[j],v[k]))**(2/(float(m)-1)) for k in range(Nclusters)]).sum() 
+    M[i,j] = 1/np.array([(euclidean(X[j],v[i])/euclidean(X[j],v[k]))**(2/(float(m)-1)) for k in range(Nclusters)]).sum()
 
  return M
 
@@ -131,29 +131,29 @@ def PC(X,cIDX):
 def CE(X,cIDX):
  M = MM(X,cIDX)
  return -(M*np.log(M)).sum()/float(X.shape[0])
- 
+
 def sm(X,cIDX,dist='euclidean'):
  Nclusters = cIDX.max()+1
  Npoints=len(X)
- 
+
 # Clusters
  A = np.array([ X[np.where(cIDX == i)] for i in range(Nclusters)])
 # Centroids
- v = np.array([ np.sum(Ai,axis = 0)/float(Ai.shape[0])  for Ai in A]) 
- dv = squareform(pdist(v, metric = dist)) 
+ v = np.array([ np.sum(Ai,axis = 0)/float(Ai.shape[0])  for Ai in A])
+ dv = squareform(pdist(v, metric = dist))
 
  aux1 = []
  for i in range(Nclusters):
   aux1.append((dv[i,dv[i].argsort()[1]])**2)
 
  M=MM(X,cIDX)
- 
+
  z = np.ndarray(shape = (Nclusters,Npoints),dtype = float)
  for i in range(Nclusters):
   for j in range(Npoints):
    z[i,j] = (euclidean(X[j],v[i])**2)*(M[i,j]**2)
  return(z.sum()/(Npoints*np.array(aux1).min()))
- 
+
 def ch(X,cIDX,dist='euclidean'):
  Nclusters = cIDX.max()+1
  Npoints=len(X)
@@ -162,7 +162,7 @@ def ch(X,cIDX,dist='euclidean'):
 
  j=0
  for i in range(cIDX.min(),cIDX.max()+1):
-  aux=np.asarray([float(b) for b in (cIDX==i)])  
+  aux=np.asarray([float(b) for b in (cIDX==i)])
   n[j]=aux.sum()
   j=j+1
 
@@ -182,7 +182,7 @@ def ch(X,cIDX,dist='euclidean'):
  for i in range(cIDX.min(),cIDX.max()+1):
   aux=np.array([(euclidean(x,v[i])**2) for x in X[cIDX==i]])
   z[i]=aux.sum()
-   
+
  ssw=z.sum()
 
  return((ssb/(Nclusters-1))/(ssw/(Npoints-Nclusters)))

optimize.py
@@ -63,10 +63,10 @@ class sim_ann:
   self.nS = 0
 
   while (True):
-   ww = self.weights_gen(3)
-   self.w1 = ww[0]
-   self.w2 = ww[1]
-   self.w3 = ww[2]
+ #  ww = self.weights_gen(3)
+   self.w1 = 1
+   self.w2 = 0
+   self.w3 = 0
    #print("{:.2} {:.2} {:.2}".format(ww[0],ww[1],ww[2]))
    si = self.Perturba(self.s.copy(),self.sd + self.sd_min)
    aux = self.f([si,self.w1,self.w2,self.w3])
@@ -79,13 +79,13 @@ class sim_ann:
    if (i > self.P) or (self.nS > self.L):
     k = 0
     if self.nS > 0:
-     while (self.fit[3] > self.hall_of_fame[k][3]):
+     while (self.fit[3] < self.hall_of_fame[k][3]):
       k = k + 1
       if k == 5:
        break
-      if k < 5:
-        self.hall_of_fame.insert(k,scipy.hstack((self.fit,self.s)))
-        self.hall_of_fame.pop()
+     if k < 5:
+      self.hall_of_fame.insert(k,scipy.hstack((self.fit,self.s)))
+      self.hall_of_fame.pop()
     break
 
   self.T = self.alpha*self.T

optimize_sa.py
@@ -51,7 +51,7 @@ else:
  nn = 0
  mm = 0
 
-N,M = 250,15
+N,M = 200,5
 
 Head = {'algo':algo,'conf':"T0,alpha,P,L = {0},{1},{2},{3}".format(conf[0],conf[1],conf[2],conf[3]),'dim':dim,"dataset":dataset}
 
@@ -78,11 +78,11 @@ if __name__ == '__main__':
  def cost_func(args):
   partial_ff = partial(ff, s = args[0])
   res = pool.map(partial_ff,cnt)
-  si = metrics.silhouette(scale(np.array(res)),np.array(Y)-1)
+  si = metrics.silhouette(scale(np.array(res)),np.array(Y)-1,distance = 'mahalanobis')
   db = metrics.db(scale(np.array(res)),np.array(Y)-1)
   ch = metrics.ch(scale(np.array(res)),np.array(Y)-1)
   w1,w2,w3 = args[1],args[2],args[3]
-  fit = 3*w1*np.median(np.abs(1.-si))+w2*db+50*w3/(ch+1e-12)
+  fit = 3*w1*(np.std(si) + np.median(np.abs(1.-si)))+w2*db+50*w3/(ch+1e-12)
 
   return (np.mean(si),db,ch,fit)
 
@@ -111,5 +111,9 @@ if __name__ == '__main__':
      print(ss)
      pickle.dump([i,w.fit,w.s],f)
     os.remove("dump_optimize_sa2.pkl")
+    print()
+    for hf in w.hall_of_fame:
+     print(hf)
+    print()
     pickle.dump(w.hall_of_fame[0],f)
     nn = 0